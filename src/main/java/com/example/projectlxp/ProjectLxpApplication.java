133ed5a255839c31f07b34e87de75f7a8cf5b6e7
Richard Cho <abelsteiger@gmail.com>
2025-11-04 12:48:07 +0900

    INITIAL COMMIT

---
+++ src/main/java/com/example/projectlxp/ProjectLxpApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectlxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLxpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLxpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectlxp/ProjectLxpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectlxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLxpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
