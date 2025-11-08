4b81457d10a98bd7d3d9f56fff0fd27af57bd9da
sanseongKo <tkstjd56@naver.com>
2025-11-08 20:31:50 +0900

    [Feature/EC] CourseId 기준 수강신청된 Course 개수 조회 쿼리메서드 추가

--- src/main/java/com/example/projectlxp/repository/enroll/EnrolledCourseRepository.java
+++ src/main/java/com/example/projectlxp/repository/enroll/EnrolledCourseRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EnrolledCourseRepository extends JpaRepository<EnrolledCourse, Long> {
+    Integer countByCourseId(Long courseId);
 }
