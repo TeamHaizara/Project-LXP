5d1abd6410c2cff4312f9a6d843823d65771604b
sanseongKo <tkstjd56@naver.com>
2025-11-08 20:08:46 +0900

    [Feature/EC] 인롤코스 컨트롤러 추가
    - Request 추가
    - 수강신청 컨트롤러 메서드 추가

---
+++ src/main/java/com/example/projectlxp/controller/enroll/EnrolledCourseController.java
@@ -0,0 +1,21 @@
+package com.example.projectlxp.controller.enroll;
+
+import com.example.projectlxp.controller.enroll.request.EnrollCourseRequest;
+import com.example.projectlxp.service.enroll.EnrolledCourseService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnrolledCourseController {
+    private final EnrolledCourseService enrolledCourseService;
+
+    public EnrolledCourseController(EnrolledCourseService enrolledCourseService) {
+        this.enrolledCourseService = enrolledCourseService;
+    }
+
+    @PostMapping("/api/learner/enroll")
+    public void enroll(@RequestBody EnrollCourseRequest request, Long userId) {
+        enrolledCourseService.enroll(request.toDto(userId));
+    }
+}
