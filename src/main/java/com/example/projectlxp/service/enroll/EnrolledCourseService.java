4d75e80b02f979f8f83ffdb665a77e0dbe596a94
sanseongKo <tkstjd56@naver.com>
2025-11-09 18:32:47 +0900

    [Feature/EC] 유저기준 인롤 코스들을 가져오는 서비스 메서드 추가

--- src/main/java/com/example/projectlxp/service/enroll/EnrolledCourseService.java
+++ src/main/java/com/example/projectlxp/service/enroll/EnrolledCourseService.java
@@ -2,11 +2,15 @@ package com.example.projectlxp.service.enroll;
 
 import com.example.projectlxp.exception.BusinessException;
 import com.example.projectlxp.exception.ExceptionCode;
+import com.example.projectlxp.model.course.Course;
+import com.example.projectlxp.model.enroll.EnrolledCourse;
 import com.example.projectlxp.repository.course.CourseRepository;
 import com.example.projectlxp.repository.enroll.EnrolledCourseRepository;
 import com.example.projectlxp.service.enroll.dto.EnrollCourseServiceDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -27,6 +31,14 @@ public class EnrolledCourseService {
         validateExistCourse(dto.courseId());
 
         enrolledCourseRepository.save(dto.toEntity());
+    }
+
+    public List<Course> getEnrolledCourses(Long userId) {
+        List<EnrolledCourse> enrolledCourses = enrolledCourseRepository.findEnrolledCoursesByUserId(userId);
+
+        return courseRepository.findByIdsAndNotDeleted(
+            enrolledCourses.stream().map(EnrolledCourse::getCourseId).toList()
+        );
     }
 
     private void validateExistUser(Long userId) {
