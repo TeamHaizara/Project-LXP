09267fbb8ca9241c212ac17c50331ccc93dffacf
hojun-lang <milky9925@gmail.com>
2025-11-10 18:02:28 +0900

    User Entity 클래스 추가

---
+++ src/main/java/com/example/projectlxp/model/user/Role.java
@@ -0,0 +1,7 @@
+package com.example.projectlxp.model.user;
+
+public enum Role {
+
+    ROLE_LEARNER,
+    ROLE_INSTRUCTOR
+}
---
+++ src/main/java/com/example/projectlxp/model/user/User.java
@@ -0,0 +1,66 @@
+package com.example.projectlxp.model.user;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column // 일단
+    private String interest;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private List<Role> roles = new ArrayList<>();
+
+    public User(String username, String password, String nickname, String interest, List<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.interest = interest;
+        this.roles = roles;
+    }
+
+    protected User() {}
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+}
