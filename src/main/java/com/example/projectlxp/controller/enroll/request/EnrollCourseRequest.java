c13b842d47fac5a81ad7dcbaaeb656f6679c7fcd
sanseongKo <tkstjd56@naver.com>
2025-11-11 15:19:52 +0900

    [Feature/Payment] api 입력값 검증 추가

--- src/main/java/com/example/projectlxp/controller/enroll/request/EnrollCourseRequest.java
+++ src/main/java/com/example/projectlxp/controller/enroll/request/EnrollCourseRequest.java
@@ -2,9 +2,12 @@ package com.example.projectlxp.controller.enroll.request;
 
 import com.example.projectlxp.model.payment.enums.PaymentMethod;
 import com.example.projectlxp.service.enroll.dto.EnrollCourseServiceDto;
+import jakarta.validation.constraints.NotNull;
 
 public record EnrollCourseRequest(
+    @NotNull(message = "강좌를 선택해 주세요.")
     Long courseId,
+    @NotNull(message = "결제 방법을 선택해 주세요.")
     PaymentMethod paymentMethod
 ) {
     public EnrollCourseServiceDto toDto(Long userId) {
