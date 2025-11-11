99afae979b65df885b0ec024ae8366c9fc9255a0
sanseongKo <tkstjd56@naver.com>
2025-11-11 11:09:23 +0900

    [Feature/Payment] 수강시 결제 로직 수행을 위한 결제 방법 추가

--- src/main/java/com/example/projectlxp/controller/enroll/request/EnrollCourseRequest.java
+++ src/main/java/com/example/projectlxp/controller/enroll/request/EnrollCourseRequest.java
@@ -1,11 +1,13 @@
 package com.example.projectlxp.controller.enroll.request;
 
+import com.example.projectlxp.model.payment.enums.PaymentMethod;
 import com.example.projectlxp.service.enroll.dto.EnrollCourseServiceDto;
 
 public record EnrollCourseRequest(
-    Long courseId
+    Long courseId,
+    PaymentMethod paymentMethod
 ) {
     public EnrollCourseServiceDto toDto(Long userId) {
-        return new EnrollCourseServiceDto(userId, courseId);
+        return new EnrollCourseServiceDto(userId, courseId, paymentMethod);
     }
 }
