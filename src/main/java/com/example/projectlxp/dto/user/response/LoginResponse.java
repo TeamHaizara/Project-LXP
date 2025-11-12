901b8fb9e9ea009f503f318bdfce379747766fd6
hojun-lang <milky9925@gmail.com>
2025-11-12 10:46:44 +0900

    [fix] 토큰 발급 DTO 생성

---
+++ src/main/java/com/example/projectlxp/dto/user/response/LoginResponse.java
@@ -0,0 +1,41 @@
+// src/main/java/com/example/projectlxp/dto/user/LoginResponse.java
+package com.example.projectlxp.dto.user;
+
+import com.example.projectlxp.dto.user.response.UserResponse;
+import com.example.projectlxp.model.user.User;
+
+public class LoginResponse {
+
+    private String accessToken;
+    private String tokenType;
+    private long expiresIn;     // 토큰 만료 시간(초 단위)
+    private UserResponse user;  // 사용자 정보
+
+    public LoginResponse(String accessToken, String tokenType, long expiresIn, UserResponse user) {
+        this.accessToken = accessToken;
+        this.tokenType = tokenType;
+        this.expiresIn = expiresIn;
+        this.user = user;
+    }
+
+    public static LoginResponse of(String accessToken, long expiresIn, User user) {
+        return new LoginResponse(accessToken, "Bearer", expiresIn / 10000000, UserResponse.from(user));
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public UserResponse getUser() {
+        return user;
+    }
+
+}
