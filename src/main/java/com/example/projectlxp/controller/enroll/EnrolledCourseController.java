f9cc70f92df4f3f04069e4cad08bf02ea455c6d9
sanseongKo <tkstjd56@naver.com>
2025-11-13 12:12:38 +0900

    [feature/enroll] 유저 디테일 주입받는 코드 추가

--- src/main/java/com/example/projectlxp/controller/enroll/EnrolledCourseController.java
+++ src/main/java/com/example/projectlxp/controller/enroll/EnrolledCourseController.java
@@ -3,9 +3,11 @@ package com.example.projectlxp.controller.enroll;
 import com.example.projectlxp.controller.enroll.request.EnrollCourseRequest;
 import com.example.projectlxp.controller.enroll.response.EnrolledCoursesResponse;
 import com.example.projectlxp.service.enroll.EnrolledCourseService;
+import com.example.projectlxp.service.user.dto.CustomUserDetails;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,14 +22,17 @@ public class EnrolledCourseController {
     }
 
     @PostMapping("/api/learner/enroll")
-    public ResponseEntity<Void> enroll(@Valid @RequestBody EnrollCourseRequest request, Long userId) {
-        enrolledCourseService.enroll(request.toDto(userId));
+    public ResponseEntity<Void> enroll(
+        @Valid @RequestBody EnrollCourseRequest request,
+        @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        enrolledCourseService.enroll(request.toDto(userDetails.getUserId()));
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/api/learner/enrolled-courses")
-    public EnrolledCoursesResponse getEnrolledCourses(Long userId) {
-        return EnrolledCoursesResponse.of(enrolledCourseService.getEnrolledCourses(userId));
+    public EnrolledCoursesResponse getEnrolledCourses(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return EnrolledCoursesResponse.of(enrolledCourseService.getEnrolledCourses(userDetails.getUserId()));
     }
 }
